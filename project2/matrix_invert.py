8d4dee56bb6bfb834d10ca2cf82aaed587dd7496
Pseudo-Swetoniusz <jbtokarska@gmail.com>
2023-03-25 21:37:10 +0100

    project2 - inverse matrix

---
+++ project2/matrix_invert.py
@@ -0,0 +1,101 @@
+import numpy as np
+
+from project2.mat_mul_op_count import matrix_multiplication_operation_count
+from project2.matrix_operations import subtract, matrix_multiplication, add
+
+def identity(n):
+    return np.identity(n)
+
+def det(A):
+    return np.linalg.det(A)
+
+def inverse_matrix(A, l):
+    n = len(A)
+
+    if n == 1:
+        return 1/np.array(A)
+
+    if det(A) == 0:
+        return "Macierz nie ma macierzy odwrotnej"
+
+    mid = n // 2
+
+    A11 = [row[:mid] for row in A[:mid]]
+    A12 = [row[mid:] for row in A[:mid]]
+    A21 = [row[:mid] for row in A[mid:]]
+    A22 = [row[mid:] for row in A[mid:]]
+
+
+    invert_A11 = inverse_matrix(A11, l)
+    S22 = subtract(A22, matrix_multiplication(matrix_multiplication(A21, invert_A11, l), A12, l))
+    invert_S22 = inverse_matrix(S22, l)
+    B11 = matrix_multiplication(invert_A11, add(identity(len(invert_A11)),
+                                                matrix_multiplication(matrix_multiplication(A12, invert_S22, l),
+                                                                      matrix_multiplication(A21, invert_A11, l), l)), l)
+    B12 = matrix_multiplication((-1) * np.array(invert_A11), matrix_multiplication(A12, invert_S22, l), l)
+    B21 = matrix_multiplication((-1) * np.array(invert_S22), matrix_multiplication(A21, invert_A11, l), l)
+    B22 = invert_S22
+
+    B = [[0 for _ in range(n)] for _ in range(n)]
+    for i in range(n // 2):
+        for j in range(n // 2):
+            B[i][j] = B11[i][j]
+            B[i][j + n // 2] = B12[i][j]
+            B[i + n // 2][j] = B21[i][j]
+            B[i + n // 2][j + n // 2] = B22[i][j]
+
+    return np.array(B)
+
+def invert_matrix_op_count(A, l):
+    op_count = 0
+    n = len(A)
+
+    if n == 1:
+        return 1
+
+    if det(A) == 0:
+        return "Macierz nie ma macierzy odwrotnej"
+
+    mid = n // 2
+    A11 = [row[:mid] for row in A[:mid]]
+    A12 = [row[mid:] for row in A[:mid]]
+    A21 = [row[:mid] for row in A[mid:]]
+    A22 = [row[mid:] for row in A[mid:]]
+
+    invert_A11 = inverse_matrix(A11, l)
+    op_count += invert_matrix_op_count(A11, l)
+    tmp_1 = matrix_multiplication(A21, invert_A11, l)
+    S22 = subtract(A22, matrix_multiplication(tmp_1, A12, l))
+    op_count = op_count + len(A22)**2 + matrix_multiplication_operation_count(A21, invert_A11, l) + matrix_multiplication_operation_count(tmp_1,A12,l)
+    invert_S22 = inverse_matrix(S22, l)
+    op_count += invert_matrix_op_count(S22, l)
+    tmp_2 = matrix_multiplication(A21, invert_A11, l)
+    op_count += matrix_multiplication_operation_count(A21, invert_A11, l)
+    tmp_3 = matrix_multiplication(A12, invert_S22, l)
+    op_count += matrix_multiplication_operation_count(A12, invert_S22, l)
+    tmp_4 = matrix_multiplication(tmp_3, tmp_2, l)
+    op_count += matrix_multiplication_operation_count(tmp_3, tmp_2, l)
+    tmp_5 = add(identity(len(invert_A11)), tmp_4)
+    op_count += len(invert_A11) ** 2
+    B11 = matrix_multiplication(invert_A11, tmp_5, l)
+    op_count += matrix_multiplication_operation_count(invert_A11, tmp_5, l)
+    tmp_6 = matrix_multiplication(A12, invert_S22, l)
+    op_count += matrix_multiplication_operation_count(A12, invert_S22, l)
+    B12 = matrix_multiplication((-1) * np.array(invert_A11), tmp_6, l)
+    op_count += matrix_multiplication_operation_count((-1) * np.array(invert_A11), tmp_6,l)
+    tmp_7 = matrix_multiplication(A21, invert_A11, l)
+    op_count += matrix_multiplication_operation_count(A21, invert_A11, l)
+    B21 = matrix_multiplication((-1) * np.array(invert_S22), tmp_7, l)
+    op_count += matrix_multiplication_operation_count((-1) * np.array(invert_S22), tmp_7, l)
+    B22 = invert_S22
+
+    op_count = op_count + len(invert_A11)**2 + len(invert_S22)**2
+
+    return op_count
+
+
+if __name__ == '__main__':
+    M = np.array([[1, 2, 7, 2], [1, 1, 3, 3], [4, 0, 1, 0], [4, 5, 6, 4]])
+    print((-1) * M)
+    print(inverse_matrix(M, 4))
+    print(invert_matrix_op_count(M, 4))
---
+++ project2/matrix_operations.py
@@ -0,0 +1,108 @@
+import time
+import numpy as np
+
+'''
+    All multiplying methods:
+'''
+
+
+def classic_multiplication(matrix1, matrix2):
+    result = [[0 for _ in range(len(matrix2[0]))] for _ in range(len(matrix1))]
+    for i in range(len(matrix1)):
+        for j in range(len(matrix2[0])):
+            for k in range(len(matrix2)):
+                result[i][j] += matrix1[i][k] * matrix2[k][j]
+    return (result)
+
+
+def binet(A, B, l):
+    # Sprawdzenie, czy liczba kolumn macierzy A jest równa liczbie wierszy macierzy B
+    if len(A[0]) != len(B):
+        return None
+
+    # Sprawdzenie, czy macierze A i B są macierzami kwadratowymi
+    if len(A) != len(A[0]) or len(B) != len(B[0]):
+        return None
+
+    n = len(A)
+
+    # Jeśli n = 1 to zwróć iloczyn pojedynczych elementów
+    if n == 1:
+        return [[A[0][0] * B[0][0]]]
+
+    # podział macierzy na 4 'podmacierze' - bloki
+    mid = n // 2
+
+    A11 = [row[:mid] for row in A[:mid]]
+    A12 = [row[mid:] for row in A[:mid]]
+    A21 = [row[:mid] for row in A[mid:]]
+    A22 = [row[mid:] for row in A[mid:]]
+    B11 = [row[:mid] for row in B[:mid]]
+    B12 = [row[mid:] for row in B[:mid]]
+    B21 = [row[:mid] for row in B[mid:]]
+    B22 = [row[mid:] for row in B[mid:]]
+
+    # Wywołanie rekurencyjne
+    if len(A11) > 2 ** l:
+        C11 = add(binet(A11, B11, l), binet(A12, B21, l))
+        C12 = add(binet(A11, B12, l), binet(A12, B22, l))
+    else:
+        C11 = add(classic_multiplication(A11, B11), classic_multiplication(A12, B21))
+        C12 = add(classic_multiplication(A11, B12), classic_multiplication(A12, B22))
+    if len(A21) > 2 ** l:
+        C21 = add(binet(A21, B11, l), binet(A22, B21, l))
+        C22 = add(binet(A21, B12, l), binet(A22, B22, l))
+    else:
+        C21 = add(classic_multiplication(A21, B11), classic_multiplication(A22, B21))
+        C22 = add(classic_multiplication(A21, B12), classic_multiplication(A22, B22))
+
+    # Połączenie czterech podmacierzy w jedną macierz wynikową
+    C = [[0 for _ in range(n)] for _ in range(n)]
+    for i in range(n // 2):
+        for j in range(n // 2):
+            C[i][j] = C11[i][j]
+            C[i][j + n // 2] = C12[i][j]
+            C[i + n // 2][j] = C21[i][j]
+            C[i + n // 2][j + n // 2] = C22[i][j]
+
+    return C
+
+
+"""
+    Multiplication program:
+"""
+
+
+def matrix_multiplication(A, B, l):
+    if len(A) > 2 ** l:
+        return binet(A, B, l)
+    else:
+        return classic_multiplication(A, B)
+
+
+"""
+    Helper function:
+"""
+
+
+def add(A, B):
+    return [[A[i][j] + B[i][j] for j in range(len(A[0]))] for i in range(len(A))]
+
+
+def subtract(A, B):
+    return [[A[i][j] - B[i][j] for j in range(len(A[0]))] for i in range(len(A))]
+
+
+def generate_matrix(n):
+    array = np.random.uniform(5, 20, size=(n, n))
+    return array
+
+
+if __name__ == '__main__':
+    matrix1 = generate_matrix(4)
+    matrix2 = generate_matrix(4)
+
+    start = time.time()
+    print(matrix_multiplication(matrix1, matrix2, 1))
+    end = time.time()
+    print(end - start)
